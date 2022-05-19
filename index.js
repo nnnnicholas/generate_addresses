ca1f2ce4e6d4ecf7e8fa4f1a3cf2462cd39a56a1
nicholas <nnnnicholas@users.noreply.github.com>
2022-05-18 23:39:35 -0400

    works

---
+++ index.js
@@ -0,0 +1,14 @@
+var ethers = require('ethers');  
+var crypto = require('crypto');
+var fs = require('fs');
+
+var addresses = [];
+
+for (i =0; i<500; i++ ){
+    var id = crypto.randomBytes(32).toString('hex');
+    var privateKey = "0x"+id;
+    var wallet = new ethers.Wallet(privateKey);
+    addresses.push(wallet.address);
+}
+
+fs.writeFileSync('addresses.csv', addresses.join('\n'));
