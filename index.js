8c48ed8a7ff315c22d938c81217ae8e72120904b
nicholas <nnnnicholas@users.noreply.github.com>
2022-05-19 10:34:30 -0400

    generate 400 (10k over 25 txs)

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 
 var addresses = [];
 
-for (i =0; i<500; i++ ){
+for (i =0; i<400; i++ ){
     var id = crypto.randomBytes(32).toString('hex');
     var privateKey = "0x"+id;
     var wallet = new ethers.Wallet(privateKey);
